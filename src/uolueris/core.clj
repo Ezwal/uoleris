9fd58d3c33ff828a2862273022d295c4fd178c7d
Ezwal <ezwal@github.com>
2020-03-09 18:56:52 +0100

    Add spec

--- src/uolueris/core.clj
+++ src/uolueris/core.clj
@@ -2,27 +2,28 @@
   (:import [java.net IDN])
   (:gen-class))
 
-;; EXAMPLE : korean unicode url : 실례.테스트
-;; not till 1<<8 cos' there is not interesting char after 122 (\z)
+;; Punycode EXAMPLE : korean unicode url : 실례.테스트 παράδειγμα.δοκιμή правительство.рф
+
+;; not till 1<<8 cos' there is no char used by puny-code charset (after 122 = \z)
 (def byte-masks
   (map #(bit-shift-left 1 %) (range 7)))
 
-(defn toggle-bit-mask [char-val]
+(defn toggle-bit-mask [^Integer char-val]
   (fn [mask]
     (bit-and 0xff
              ((if (bit-test char-val mask)
                bit-clear
                bit-set) char-val mask))))
 
-(defn bit-squatting [c]
+(defn bit-squatting [^Character c]
   (let* [char-val (int c)
         toggler (toggle-bit-mask char-val)]
     (map
      toggler
      byte-masks)))
 
-(def allowed-reg #"[\.\-A-Za-z0-9]")
-(defn uniq-squatting-char [c]
+(def allowed-reg #"[.\-A-Za-z0-9]")
+(defn uniq-squatting-char [^Character c]
   (filter #(and
             (not= c %)
             (re-matches allowed-reg (str %)))
@@ -32,13 +33,13 @@
 
 (def mem-uniq-squatting-char (memoize uniq-squatting-char))
 
-(defn replace-id-by-squat [s n]
+(defn replace-id-by-squat [^String s ^Integer n]
   (fn [squat]
     (let [sb (StringBuilder. s)
           squat-str (str squat)]
       (str (.replace sb n (inc n) squat-str)))))
 
-(defn single-bit-squatting [name]
+(defn single-bit-squatting [^String name]
   (flatten (map
             (fn [idx]
               (map (replace-id-by-squat name idx)
